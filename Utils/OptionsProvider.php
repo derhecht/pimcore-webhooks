3b07af72d0a92c0ef25ff1eb872a25fb38662172
j.berlin <j.berlin@ewerk.com>
2024-07-05 11:50:18 +0200

    Fix object definition Fix functions signatures of OptionsProvider.php

--- Utils/OptionsProvider.php
+++ Utils/OptionsProvider.php
@@ -2,49 +2,52 @@
 
 namespace WebHookBundle\Utils;
 
+use Pimcore\Model\DataObject\ClassDefinition;
 use Pimcore\Model\DataObject\ClassDefinition\Data;
 use Pimcore\Model\DataObject\ClassDefinition\DynamicOptionsProvider\MultiSelectOptionsProviderInterface;
-use Pimcore\Model\DataObject\ClassDefinition;
 
 
 class OptionsProvider implements MultiSelectOptionsProviderInterface
 {
     /**
-     * @param array $context 
-     * @param Data $fieldDefinition 
+     * @param array $context
+     * @param Data $fieldDefinition
      * @return array
      */
 
-    public function getOptions($context, $fieldDefinition) {
+    public function getOptions($context, $fieldDefinition): array
+    {
         $classesList = new ClassDefinition\Listing();
         $classesList->setOrderKey('name');
         $classesList->setOrder('asc');
         $classesList->setCondition('name NOT LIKE ?', $context['class']->getName() . '%');
-        
+
         $classes = $classesList->load();
         $result = array();
         foreach ($classes as $class) {
-            $result[] = array("key" => $class->getName() , "value" => $class->getName());
-        }        
+            $result[] = array("key" => $class->getName(), "value" => $class->getName());
+        }
         return $result;
     }
 
     /**
-     * Returns the value which is defined in the 'Default value' field  
-     * @param array $context 
-     * @param Data $fieldDefinition 
+     * Returns the value which is defined in the 'Default value' field
+     * @param array $context
+     * @param Data $fieldDefinition
      * @return mixed
      */
-    public function getDefaultValue($context, $fieldDefinition) {
-        return null;//$fieldDefinition->getDefaultValue();
+    public function getDefaultValue($context, $fieldDefinition)
+    {
+        return null;
     }
 
     /**
-     * @param array $context 
-     * @param Data $fieldDefinition 
+     * @param array $context
+     * @param Data $fieldDefinition
      * @return bool
      */
-    public function hasStaticOptions($context, $fieldDefinition) {
+    public function hasStaticOptions($context, $fieldDefinition): bool
+    {
         return true;
     }
 
